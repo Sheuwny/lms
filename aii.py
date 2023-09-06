5307fb8ae4d92cf1244d9073b1757b07e23e014d
Sheuwny <143084566+Sheuwny@users.noreply.github.com>
2023-09-06 14:55:24 +0530

    Add files via upload

---
+++ aii.py
@@ -0,0 +1,82 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import subprocess
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >=0 and hour <12:
+        speak("Good Morning!")
+
+    elif hour>= 12 and hour <18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am noona. Please tell me how may I help you")
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'gpa ambota' in query:
+            webbrowser.open("gpambota.edu.in")
+        elif 'play music' in query:
+            music_dir = 'C:\Users\ritan\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(" the time is {strTime}")
+        elif 'open code' in query:
+            codePath = "C:\\Users\\ritan\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code"
+            os.startfile(codePath)
+        elif 'hello' in query:
+            speak('hello everyone how are you doing')
+        elif'who are you' in query:
+            speak('i am voice assistant created by kajal and kavita')
+        elif 'exit' in query:
+            speak('ok seee you later')
+            exit()
+        elif'restart' in query:
+            subprocess.call(["shutdown","/r"])
